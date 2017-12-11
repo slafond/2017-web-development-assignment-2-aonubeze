0bf78ee3f7b1f0ed55f05f229b9ac213e23050a4
augustio <augustine.onubeze@metropolia.fi>
2017-12-11 17:22:48 +0200

    Use Javascript promise library

--- scripts/foli-buses-routes.js
+++ scripts/foli-buses-routes.js
@@ -12,14 +12,17 @@ function drawMap(data) {
 
 $(document).ready(function(){
     var busLines;
-    $.getJSON("https://cors-anywhere.herokuapp.com/http://data.foli.fi/gtfs/routes", function(res){
-        busLines = res.map(function(route){
-            return {route_id: route.route_id, route_name: route.route_long_name, route_ref: route.route_short_name};
+    getJSON("https://cors-anywhere.herokuapp.com/http://data.foli.fi/gtfs/routes")
+        .then(function(res){
+            busLines = res.map(function(route){
+                return {route_id: route.route_id, route_name: route.route_long_name, route_ref: route.route_short_name};
+            });
+            $.each(busLines, function(i, route){
+                $("#bus-line-sel").append("<option " + "value=" + "'" + route.route_id + "'" + ">" + route.route_ref + ": " + route.route_name + "</option>");
+            });
+        }, function(err){
+            alert(err.statusText);
         });
-        $.each(busLines, function(i, route){
-            $("#bus-line-sel").append("<option " + "value=" + "'" + route.route_id + "'" + ">" + route.route_ref + ": " + route.route_name + "</option>");
-        });
-    });
     map = drawMap(turku_lat_long);
     $("#show-route").click(function(){
         var route_id = Number($("#bus-line-sel").val().trim());
@@ -42,53 +45,70 @@ $(document).ready(function(){
 });
 
 function showBusesLocation(route_id){
-    $.getJSON("https://cors-anywhere.herokuapp.com/http://data.foli.fi/siri/vm/pretty", function(vm){
-        if(vm.status !== "OK"){
-            alert("Data not ready, try again");
-        }
-        var vehicles = vm.result.vehicles;
-        var vehicleIds = Object.keys(vehicles);
-        var vehiclesInRoute = vehicleIds.filter(function(v_id){
-            return vehicles[v_id].publishedlinename === route_id;
-        });
-        oldMarkers.forEach(function(m){
-            m.setMap(null);
-        });
-        oldMakers = [];
-        vehiclesInRoute.forEach(function(v){
-            var bus = vehicles[v];
-            var myLatlng = new google.maps.LatLng(bus.latitude,bus.longitude);
-            var marker = new google.maps.Marker({
-                position: myLatlng,
-                title: v
+    getJSON("https://cors-anywhere.herokuapp.com/http://data.foli.fi/siri/vm/pretty")
+        .then(function(vm){
+            var vehicles = vm.result.vehicles;
+            var vehicleIds = Object.keys(vehicles);
+            var vehiclesInRoute = vehicleIds.filter(function(v_id){
+                return vehicles[v_id].publishedlinename === route_id;
             });
-            marker.setMap(map);
-            oldMarkers.push(marker);
-        })
-    })
+            oldMarkers.forEach(function(m){
+                m.setMap(null);
+            });
+            oldMakers = [];
+            vehiclesInRoute.forEach(function(v){
+                var bus = vehicles[v];
+                var myLatlng = new google.maps.LatLng(bus.latitude,bus.longitude);
+                var marker = new google.maps.Marker({
+                    position: myLatlng,
+                    title: v
+                });
+                marker.setMap(map);
+                oldMarkers.push(marker);
+            });
+        },
+        function(err){
+            alert(err.statusText);
+        });
 }
 
 
 function showRoute(route_id){
-    $.getJSON("https://cors-anywhere.herokuapp.com/http://data.foli.fi/gtfs/v0/20171130-162538/trips/route/"+route_id, function(trips){
-        var index = Math.floor((Math.random() * trips.length) + 1);
-        var shape_id = trips[index].shape_id;
-        $.getJSON("https://cors-anywhere.herokuapp.com/http://data.foli.fi/gtfs/v0/20171130-162538/shapes/"+shape_id, function(shape){
-            var pathCoordinates = shape.map(function(line){
-                return {lat: line.lat, lng: line.lon};
-            });
-            busPath = new google.maps.Polyline({
-                path: pathCoordinates,
-                geodesic: true,
-                strokeColor: '#000FF',
-                strokeOpacity: 1.0,
-                strokeWeight: 2
-            });
-            if(oldBusPath){
-                oldBusPath.setMap(null);
-            }
-            busPath.setMap(map);
-            oldBusPath = busPath;
+    getJSON("https://cors-anywhere.herokuapp.com/http://data.foli.fi/gtfs/v0/20171130-162538/trips/route/"+route_id)
+        .then(function(data){
+            var index = Math.floor((Math.random() * data.length) + 1);
+            return data[index].shape_id;
+        })
+        .then(function(shape_id){
+            getJSON("https://cors-anywhere.herokuapp.com/http://data.foli.fi/gtfs/v0/20171130-162538/shapes/"+shape_id)
+                .then(function(shape){
+                    var pathCoordinates = shape.map(function(line){
+                        return {lat: line.lat, lng: line.lon};
+                    });
+                    busPath = new google.maps.Polyline({
+                        path: pathCoordinates,
+                        geodesic: true,
+                        strokeColor: '#000FF',
+                        strokeOpacity: 1.0,
+                        strokeWeight: 2
+                    });
+                    if(oldBusPath){
+                        oldBusPath.setMap(null);
+                    }
+                    busPath.setMap(map);
+                    oldBusPath = busPath;
+                })
         });
+}
+
+function getJSON(url){
+    return new Promise(function(resolve, reject){
+        $.getJSON(url)
+            .done(function(data){
+                resolve(data);
+            })
+            .fail(function(err){
+                reject(err);
+            });
     });
 }
